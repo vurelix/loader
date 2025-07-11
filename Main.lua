2ed5b21a11ce63440434da62db2a5a7e7939c4da
Kenneth Nguyen <107379616+vurelix@users.noreply.github.com>
2025-07-11 18:57:08 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,5 @@
 local Library = 'https://raw.githubusercontent.com/vurelix/loader/refs/head/main/'
 local Url = Library .. game.PlaceId .. '.lua'
 
-pcall(function()
-    loadstring(game:HttpGet(Url))()
-end)
+loadstring(game:HttpGet(Url))()
+
