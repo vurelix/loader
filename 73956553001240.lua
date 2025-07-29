3345f3898e8534f074fd741228f2798f596379b4
Kenneth Nguyen <107379616+vurelix@users.noreply.github.com>
2025-07-29 14:59:34 -0500

    Update 73956553001240.lua

--- 73956553001240.lua
+++ 73956553001240.lua
@@ -1,4 +1,2 @@
-local onAuto = loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/autoExecute.lua'))()
 script_key="WQKDGYrBDNANPkYtIfdXtljRDVHwwyLl";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/237461d74ce900b286257a80eee4ef1c.lua"))()
-onAuto('loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/237461d74ce900b286257a80eee4ef1c.lua"))()')
