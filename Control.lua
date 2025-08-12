aee43ebdd9728aeaee4424fa403f91a41007890f
Kenneth Nguyen <107379616+vurelix@users.noreply.github.com>
2025-08-11 19:42:55 -0500

    Update Control.lua

--- Control.lua
+++ Control.lua
@@ -1,11 +1,7 @@
 local Script = 'Autofarm'
 
 if Script == 'Autofarm' then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/vurelix/loader/refs/heads/main/Napoleon%20Hub.lua'))()
-    task.wait(15)
-    setfpscap(10)
-    UserSettings().GameSettings.MasterVolume = 0
-    game:GetService('RunService'):Set3dRenderingEnabled(false)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/vurelix/loader/refs/heads/main/Napoleon%20Hub.lua'))()   
 elseif Script == 'Rollback' then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/vurelix/loader/refs/heads/main/73956553001240.lua'))()
 end
