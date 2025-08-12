c4732284afc86963157a473b067f7693bcbe7a60
Kenneth Nguyen <107379616+vurelix@users.noreply.github.com>
2025-08-11 19:23:27 -0500

    Update Control.lua

--- Control.lua
+++ Control.lua
@@ -1,7 +1,8 @@
 local Script = 'Autofarm'
 
 if Script == 'Autofarm' then
-
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/vurelix/loader/refs/heads/main/Napoleon%20Hub.lua'))()
 elseif Script == 'Rollback' then
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/vurelix/loader/refs/heads/main/Napoleon%20Hub.lua'))()
 
 end
