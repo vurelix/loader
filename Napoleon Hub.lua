7cbe6a0ed32f9fa1b51bf65b5b5faf46864ace5c
Kenneth Nguyen <107379616+vurelix@users.noreply.github.com>
2025-08-11 19:37:22 -0500

    Update Napoleon Hub.lua

--- Napoleon Hub.lua
+++ Napoleon Hub.lua
@@ -1,7 +1,6 @@
 local onAuto = loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/autoExecute.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/src.lua'))()
-onAuto("loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/src.lua'))()")
 onAuto("setfpscap(10)")
 onAuto("UserSettings().GameSettings.MasterVolume = 0")
 onAuto("game:GetService('RunService'):Set3dRenderingEnabled(false)")
-
+loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/src.lua'))()
+onAuto("loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/src.lua'))()")
