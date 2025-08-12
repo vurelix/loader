98d656d541be76677511c950237ba78ca0032d5a
Kenneth Nguyen <107379616+vurelix@users.noreply.github.com>
2025-08-11 19:22:56 -0500

    Create Napoleon Hub.lua

---
+++ Napoleon Hub.lua
@@ -0,0 +1,3 @@
+local onAuto = loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/autoExecute.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/src.lua'))()
+onAuto("loadstring(game:HttpGet('https://raw.githubusercontent.com/raydjs/napoleonHub/refs/heads/main/src.lua'))()")
